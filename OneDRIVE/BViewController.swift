61191ac513f6cb386aaf8ae122bd2630699b0142
Anna <piladzisi@gmail.com>
2019-04-03 17:46:12 +0300

    icons

---
+++ OneDRIVE/BViewController.swift
@@ -0,0 +1,27 @@
+//
+//  BViewController.swift
+//  OneDRIVE
+//
+//  Created by anna.sibirtseva on 03/04/2019.
+//  Copyright © 2019 anna.sibirtseva. All rights reserved.
+//
+
+import UIKit
+import WebKit
+
+class BViewController: UIViewController {
+    
+    @IBOutlet weak var myWebView: WKWebView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.title = "Sign In"
+        
+        let url = URL(string: "https://onedrive.live.com/about/signin/")
+        let request = URLRequest(url: url!)
+        myWebView.load(request)
+    }
+    }
+    
+    
+
